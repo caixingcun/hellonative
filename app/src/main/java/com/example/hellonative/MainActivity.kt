d5f2b78c4766a683942457e7ff25b3f6f46b9d34
caixc <caixc@anchnet.com>
2023-02-16 11:50:43 +0800

    [feat]: 开屏即Flutter场景下 通过MethodChannel 实现Flutter 原生通讯

--- app/src/main/java/com/example/hellonative/MainActivity.kt
+++ app/src/main/java/com/example/hellonative/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.example.hellonative
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterView
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FlutterActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
 
-        findViewById<TextView>(R.id.home).setOnClickListener {
-            startActivity(FlutterActivity.createDefaultIntent(this))
-        }
-        findViewById<TextView>(R.id.second).setOnClickListener {
-            startActivity(FlutterActivity.withNewEngine().initialRoute("/second").build(this))
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        Log.d("tag","configureFlutterEngine")
+        var channel =   MethodChannel(flutterEngine.dartExecutor.binaryMessenger,"flutter_channel")
+        channel.setMethodCallHandler { call, result ->
+            // flutter 调原生
+            if(call.method == "getPlatformVersion"){
+                // 原生回复flutter
+                result.success("android ${Build.VERSION.SDK_INT}")
+            }else {
+                result.notImplemented()
+            }
         }
     }
+
 }
